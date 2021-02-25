92793d21ec1bf467fd68553347288b0885d4098b
Flo Orford <floorford@pop-os.localdomain>
2021-02-25 12:58:01 +0100

    added a ts config option to remove an error I was getting in my class intialisation, also created a classes.ts to demo how class declaration differs in ts

---
+++ src/classes.ts
@@ -0,0 +1,67 @@
+// A TS class - everything works as in JS, just have to remember to type things!
+class Robot {
+  public colour: string;
+
+  // static things are publicly available unless stated otherwise
+  static availableColours = ["pink", "white"];
+  static isColourAvailable(colour: string): boolean {
+    return Robot.availableColours.includes(colour);
+  }
+
+  constructor(public name: string) {
+    // this actually gets set automatically by TS, but nice to write out for us
+    this.name = name;
+  }
+
+  set newName(value: string) {
+    this.name = value;
+  }
+
+  set newColour(colour: string) {
+    // remember static properties have to accepted via the class, not using this.
+    if (!Robot.isColourAvailable) {
+      throw new Error("Colour is not available");
+    }
+
+    this.colour = colour;
+  }
+
+  get robotName() {
+    return this.name;
+  }
+
+  askName() {
+    console.log(`My name is ${this.name}`);
+  }
+
+  move(distance: number) {
+    console.log(`${this.name} moved ${distance} meters`);
+  }
+}
+
+const robot = new Robot("BeepBoop");
+robot.askName(); // My name is BeepBoop
+
+class FlyingRobot extends Robot {
+  jetpackSize: number;
+
+  constructor(name: string, jetpackSize: number) {
+    super(name);
+
+    this.jetpackSize = jetpackSize;
+  }
+
+  move(distance: number) {
+    console.log(`${this.name} is flying`);
+    super.move(distance);
+  }
+}
+
+const flyingRobot = new FlyingRobot("Neil", 15);
+flyingRobot.move(10); // Neil is flying; Neil moved 10 metres
+
+// MODIFIERS: public, private, protected, redaonly
+// if nothing specified public is default
+// private: only accessible within the class where you're doing your declaration
+// protected: only accessible from the defined class or subclass, consumer can't access from outside
+// readonly: can't be modified
